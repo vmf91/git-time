3cef952267885cbd2f164e645d636425cce115f3
João Gabriel <jgabsfernandes@gmail.com>
2021-08-05 21:41:24 -0300

    Set current dir as default path

--- bin/git-time.js
+++ bin/git-time.js
@@ -2,7 +2,7 @@
 var argv = require('minimist')(process.argv.slice(2));
 
 // If help or bad usage
-if (typeof argv.help == 'boolean' || typeof argv.h == 'boolean' || typeof argv._[0] == 'undefined') {
+if (typeof argv.help == 'boolean' || typeof argv.h == 'boolean') {
   console.log('\nUsage: git-time <path>\n\nWhere <path> is the path of your Git repository.\n')
   console.log('Options:\n')
   console.log('  -h, --help\toutput usage information')
@@ -16,7 +16,7 @@ if (typeof argv.help == 'boolean' || typeof argv.h == 'boolean' || typeof argv._
 const { exec } = require('child_process');
 const _cliProgress = require('cli-progress');
 
-var dir = argv._[0];
+var dir = argv._[0] || '.';
 if (dir == '.') {
   dir = process.cwd()
 }
